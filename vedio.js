c9555560a4855b68cceec9965d9cb620bf6b1bb9
lordtech-101 <lordtech101@gmail.com>
2025-11-30 14:05:56 +0100

    Add files via upload

---
+++ vedio.js
@@ -0,0 +1,11 @@
+[
+  {
+    "title": "Sample Video 1",
+    "link": "https://www.youtube.com/embed/dQw4w9WgXcQ"
+  },
+  {
+    "title": "New Video",
+    "link": "https://www.youtube.com/embed/NEW_VIDEO_LINK"
+  }
+]
+
